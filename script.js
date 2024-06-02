8f795cf97dcaa1574cff652b986e0655d84fe475
DietMilk4th <khaled.nsouli2004@gmail.com>
2024-06-03 00:17:17 +0300

    finished skills

--- script.js
+++ script.js
@@ -1,49 +1,89 @@
 const htmlPath=document.getElementsByClassName('HTMLPATH');
+
+let statuses={
+    c:'hidden',
+    java:'hidden',
+    python:'hidden'
+};
+
+
 function RectClick1(revealed, status){
 
     for(let i=0; i<revealed.length; i++){
         revealed[i].style.display='block';
     }
-    status="revealed";
-    
+    statuses[status]='revealed';
+    console.log(statuses[status]);
+    checkHTML(statuses.c, statuses.java,statuses.python);
    
 }
 
-function checkHTML(C,java,python){
-    if(C==="revealed" && java==="revealed" && python==="revealed"){
-        htmlPath.style.display='block';
+
+function RectClick2(status){
+    statuses[status]='revealed';
+    console.log(statuses[status]);
+    checkHTML(statuses.c,statuses.java, statuses.python);
+   
+}
+
+function RectClick3(revealed){
+    for(let i=0;i<revealed.length; i++){
+        revealed[i].style.display='block';
+    }
+}
+
+function checkHTML(C, java, python){
+    if(C==='revealed' && java==='revealed' && python==='revealed'){
+        
+        for(let i=0; i<htmlPath.length; i++){
+            htmlPath[i].style.display='block';
+        }
      }
 }
 
-function RectClick2(status){
-    status="revealed";
-    console.log(status);
-
-   
-}
 
 const RectC=document.getElementById('cRect');
-let cStatus="hidden";
+
+const javaRect=document.getElementById('java');
+const pythonRect=document.getElementById('python');
+
 const Cchildren=document.getElementsByClassName('Cplus');
 
 RectC.addEventListener('click', ()=>{
-   RectClick1(Cchildren,cStatus);
-   checkHTML(cStatus,javaStatus,pythonStatus);
+   RectClick1(Cchildren,'c');
 });
 
-const javaRect=document.getElementById('java');
-let javaStatus="hidden";
-const pythonRect=document.getElementById('python');
-let pythonStatus="hidden";
+
+
+
+
 
 javaRect.addEventListener('click', ()=>{
-    RectClick2(javaStatus);
-    checkHTML(cStatus,javaStatus,pythonStatus);
+    RectClick2('java');
+    
 })
 
 pythonRect.addEventListener('click', ()=>{
-    RectClick2(pythonStatus);
-    checkHTML(cStatus,javaStatus,pythonStatus);
+    RectClick2('python');
+    
+})
+
+const CSSJS=document.getElementsByClassName('CSSJS');
+const htmlR=document.getElementById('HTMLR');
+htmlR.addEventListener('click', ()=>{
+    RectClick3(CSSJS);
+})
+
+const CSSrect=document.getElementById('CSS');
+const CSSchildren=document.getElementsByClassName('CSSchildren');
+
+CSSrect.addEventListener('click', ()=>{
+    RectClick3(CSSchildren);
 })
 
 
+const script=document.getElementById('JS');
+const JSchildren=document.getElementsByClassName('JSchildren');
+script.addEventListener('click', ()=>{
+    RectClick3(JSchildren);
+})
